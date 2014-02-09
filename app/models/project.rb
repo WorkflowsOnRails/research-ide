1b18a383b28f00e7db95ddd2a434e61eadd844a8
Alexander Clelland <Argonanth@gmail.com>
2014-02-08 20:12:19 -0500

    Really upset at my complete lack of progress 	-For some reason the create_hypothesis is not triggering when a project is created and this stopped me from doing much of anything

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,41 @@ class Project < ActiveRecord::Base
 
   belongs_to :owner, class_name: 'User'
   belongs_to :last_updater, class_name: 'User'
+  has_many :tasks, dependent: :destroy
 
   aasm do
-    state :writing_hypothesis, initial: true
+    state :writing_hypothesis, initial: true, after_enter: :create_hypothesis
+    state :writing_literature_review, after_enter: :create_literature_review
+    state :describe_method
+    state :performing_experiment
+    state :analyze_results
+    state :drawing_conclusions
+
+    event :writing_literature_review do
+      transitions from: :writing_hypothesis,
+                  to: :writing_literature_review
+    end
+
+    event :describe_method do
+      transitions from: :writing_literature_review,
+                  to: :describe_method
+    end
+
+    event :performing_experiment do
+      transitions from: :describe_method,
+                  to: :performing_experiment
+    end
+
+    event :analyze_results do
+      transitions from: :performing_experiment,
+                  to: :analyze_results
+    end
+
+    event :drawing_conclusions do
+      transitions from: :analyze_results,
+                  to: :drawing_conclusions
+    end
+
   end
 
   validates :name, presence: true, allow_blank: false
@@ -15,4 +47,13 @@ class Project < ActiveRecord::Base
   def owned_by?(user)
     owner_id == user.id
   end
+
+  def create_hypothesis
+    Task.new(task_type: Task::TYPES[0], project_id: id, last_updater_id: owner_id, content: "").save
+  end
+
+  def create_literature_review
+    Task.new(task_type: Task::TYPES[1], project_id: id, last_updater_id: owner_id, content: "").save
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208203404) do
+ActiveRecord::Schema.define(version: 20140208215007) do
 
   create_table "projects", force: true do |t|
     t.string   "aasm_state",      null: false
