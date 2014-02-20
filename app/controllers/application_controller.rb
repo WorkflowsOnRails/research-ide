9df4a14e23a8284d20c7d23bc622ce2f746e77c2
Brendan MacDonell <brendan@macdonell.net>
2014-02-19 20:57:45 -0500

    Fix potential for redirect loops
    Some actions, such as projects#show, result in a redirect to another page.
    Unfortunately, if the user didn't have permission to visit the target page,
    then they could be redirected to the redirecting page ad infinitum.
    
    Instead, we now boot all unauthorized accesses back to the root page. In
    reality, users shouldn't encounter 401 errors anyway, so the impact is minimal.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:error] = "You are not authorized to perform this action."
-    redirect_to request.headers["Referer"] || root_path
+    redirect_to root_path
   end
 
 end
