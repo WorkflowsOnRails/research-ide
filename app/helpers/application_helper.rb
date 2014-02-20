5d30abd93f947f762fc8eb97625c15b93da530a0
Brendan MacDonell <brendan@macdonell.net>
2014-02-19 20:57:53 -0500

    Remove unused application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,23 +13,6 @@ module ApplicationHelper
     FLASH_CLASSES[level]
   end
 
-  # Helper that outputs "active" if a controller is active, or an
-  # empty string otherwise. Used to flag navigation links active
-  # when the controller they correspond to is visited.
-  #
-  # @author Brendan MacDonell
-  def nav_class_for_controller(*controller)
-    is_active = controller.include?(params[:controller])
-    is_active ? "active" : ""
-  end
-
-  # Render a datetime into a format useable by jquery.localtime
-  # @author Brendan MacDonell
-  def render_datetime(datetime, format='')
-    return nil if datetime.nil?
-    content_tag :span, datetime.iso8601, {'data-localtime-format' => format}
-  end
-
   # Helper for a row of buttons in a form rendered with our default styling
   # @author Brendan MacDonell
   def bootstrap_form_buttons(&block)
